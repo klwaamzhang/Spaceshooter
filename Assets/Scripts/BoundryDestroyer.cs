a58ecf6e0af05c99fea96830ed2b7d399f1442ce
klwaamzhang <klwaamzhang@users.noreply.github.com>
2018-03-08 23:14:50 -0500

    Animation Building

---
+++ Assets/Scripts/BoundryDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundryDestroyer : MonoBehaviour {
+
+    /*//Run when an object first enters into  collider zone
+    //Run once!
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+
+    //Runs whenever an object is inside a collider
+    //Run every frame
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+    }*/
+
+    //Run whenever an object exists the collider zone
+    //Run once
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
